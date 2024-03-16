08d1effecde1b2e4bc769e3f3c56741678c914f0
Carles <carlesmatosesg@gmail.com>
2024-03-16 21:27:38 +0100

    modify styles

--- src/pagina/proyectos.jsx
+++ src/pagina/proyectos.jsx
@@ -5,22 +5,21 @@ import DictionaryProjects from "../components/DictionaryProjects";
 
 function Proyecto_plantilla({ proyectos }) {
     return (
-        <Container className="pt-4">
+        <Container className="pt-4 mb-5">
             {proyectos.map((proyecto, index) => (
-                <Row className="pt-4 justify-content-center " key={index}>
-                    <Col bsPrefix="col-auto my-auto" style={{ width: "300px" }}>
-                        <Link to={`${proyecto.url}`}>
+                <Link to={`${proyecto.url}`} style={{textDecoration: 'none'}} >
+                    <Row className="pt-4 justify-content-center shadow rounded-4 mb-2 py-3" key={index}>
+                        <Col bsPrefix="col-auto my-auto" style={{ width: "300px" }}>
                             <Row className="g-0">
                                 <Image src={proyecto.image} alt="" style={{ objectFit: "cover" }} />
                             </Row>
-                        </Link>
-                    </Col>
-                    <Col bsPrefix="col-auto col-md-5 my-auto">
-                        <div className="h3">{proyecto.nombre}</div>
-                        <div>{proyecto.description}</div>
-                    </Col>
-                </Row>
-
+                        </Col>
+                        <Col bsPrefix="col-auto col-md-5 my-auto">
+                            <div className="h3">{proyecto.nombre}</div>
+                            <p className="text-dark ">{proyecto.description}</p>
+                        </Col>
+                    </Row>
+                </Link>
             ))}
         </Container >
     );
