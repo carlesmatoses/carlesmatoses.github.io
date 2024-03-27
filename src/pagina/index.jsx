4243b94857287fc997b140cf7534a7307d61e135
Carles <carlesmatosesg@gmail.com>
2024-03-27 14:01:12 +0100

    improve index readability

--- src/pagina/index.jsx
+++ src/pagina/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Container } from "react-bootstrap";
+import { Row, Container, Col } from "react-bootstrap";
 import TabComponent from "../components/TabComponent.jsx";
 import SocialMedia from "../components/SocialMedia.jsx";
 import { Helmet } from "react-helmet-async";
@@ -8,42 +8,36 @@ function Index() {
 
   return (
     <>
-    <Helmet><title>Carles Matoses Gimenez</title></Helmet>
-    
-    <Container>
+      <Helmet><title>Carles Matoses Gimenez</title></Helmet>
 
-      {/* <Row className="justify-content-center"> */}
-        <Row className="justify-content-center row-cols-1 mx-3">
+      <Container>
 
-          <div className="col mb-3">
-            <div className="row row-cols-1">
-              {/* <div className="col"><div className="h1 text-center">Hello!</div></div> */}
-              <div className="col"><div className="h3 fw-light text-center">My Name's</div></div>
-              <div className="col"><div className="h3 fw-normal text-center">Carles Matoses Gimenez</div></div>
-              <div className="col"><div className="fw-light text-center">And sometimes SelraK!</div></div>
+        <Container className="my-4" style={{maxWidth:"800px"}}>
+          <Row className="g-1">
+            <div className="h3 fw-normal ">About me</div>
+            <div className="fw-light ">
+              I have a <span className="fw-bolder">master degree in computer graphics</span> by the Polytechnic University of Catalonya
+              and a <span className="fw-bolder">bachelor's degree in Digital Technologies and Multimedia</span> by the Polytechnic University of Valencia.
             </div>
-          </div>
-
-          <div className="col mb-3">
-            <div className="row row-cols-1 g-2">
-              {/* <div className="col"><div className="fw-light text-center">I have a <b>master degree in computer graphics</b> by the Polytechnic University of Catalonya and a <b>bachelors degree in Digital Technologies and Multimedia</b> by the Polytechnic University of Valencia.</div></div> */}
-              <div className="col"><div className="fw-light text-center">I have a <b><u>master degree in computer graphics</u></b> by the Polytechnic University of Catalonya and a</div></div>
-              <div className="col"><div className="fw-light text-center"><b><u>bachelor's degree in Digital Technologies and Multimedia</u></b> by the Polytechnic University of Valencia</div></div>
-              <div className="col"><div className="fw-light text-center">I like photography, Visual Effects, CGI, and developing multimedia apps.</div></div>
+          </Row>
+          <Row className="mt-3">
+            <div className="fw-light ">
+              I like photography, Visual Effects, CGI, and developing multimedia apps.
             </div>
-          </div>
+          </Row>
+          <Row className="mt-3">
+            <SocialMedia size={"40px"} />
+          </Row>
+          <hr />
+        </Container>
 
-        </Row>
-      {/* </Row> */}
+        <Container className="my-3">
+          <Row >
+            <TabComponent />
+          </Row>
+        </Container>
 
-      <SocialMedia size={"40px"}/>
-
-      <hr />
-      <Row className="mt-3">
-        <TabComponent />
-      </Row>
-
-    </Container >
+      </Container >
     </>
   )
 }
